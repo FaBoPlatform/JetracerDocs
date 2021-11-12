8ac622afd07ff893a2e025a4ca6455463ad07ab4
Akira Sasaki <gclue.akira@gmail.com>
2021-11-12 22:09:42 +0900

    Deployed edb931c to master with MkDocs 1.2.3 and mike 1.1.2

--- master/js/version-select.js
+++ master/js/version-select.js
@@ -1,30 +1,10 @@
 window.addEventListener("DOMContentLoaded", function() {
-  function normalizePath(path) {
-    var normalized = [];
-    path.split("/").forEach(function(bit, i) {
-      if (bit === "." || (bit === "" && i !== 0)) {
-        return;
-      } else if (bit === "..") {
-        if (normalized.length === 1 && normalized[0] === "") {
-          // We must be trying to .. past the root!
-          throw new Error("invalid path");
-        } else if (normalized.length === 0 ||
-                   normalized[normalized.length - 1] === "..") {
-          normalized.push("..");
-        } else {
-          normalized.pop();
-        }
-      } else {
-        normalized.push(bit);
-      }
-    });
-    return normalized.join("/");
-  }
+  // This is a bit hacky. Figure out the base URL from a known CSS file the
+  // template refers to...
+  var ex = new RegExp("/?css/version-select.css$");
+  var sheet = document.querySelector('link[href$="version-select.css"]');
 
-  // `base_url` comes from the base.html template for this theme.
-  var REL_BASE_URL = base_url;
-  var ABS_BASE_URL = normalizePath(window.location.pathname + "/" +
-                                   REL_BASE_URL);
+  var ABS_BASE_URL = sheet.href.replace(ex, "");
   var CURRENT_VERSION = ABS_BASE_URL.split("/").pop();
 
   function makeSelect(options, selected) {
@@ -41,7 +21,7 @@ window.addEventListener("DOMContentLoaded", function() {
   }
 
   var xhr = new XMLHttpRequest();
-  xhr.open("GET", REL_BASE_URL + "/../versions.json");
+  xhr.open("GET", ABS_BASE_URL + "/../versions.json");
   xhr.onload = function() {
     var versions = JSON.parse(this.responseText);
 
@@ -54,20 +34,16 @@ window.addEventListener("DOMContentLoaded", function() {
       return {text: i.title, value: i.version};
     }), realVersion);
     select.addEventListener("change", function(event) {
-      window.location.href = REL_BASE_URL + "/../" + this.value + "/";
+      window.location.href = ABS_BASE_URL + "/../" + this.value;
     });
 
     var container = document.createElement("div");
     container.id = "version-selector";
+    container.className = "md-nav__item";
     container.appendChild(select);
 
-    var title = document.querySelector(".navbar-brand");
-    if (title.parentNode.classList.contains("navbar-header")) {
-      var height = window.getComputedStyle(title).getPropertyValue("height");
-      container.style.height = height;
-    }
-
-    title.parentNode.insertBefore(container, title.nextSibling);
+    var sidebar = document.querySelector(".md-nav--primary > .md-nav__list");
+    sidebar.parentNode.insertBefore(container, sidebar);
   };
   xhr.send();
 });
